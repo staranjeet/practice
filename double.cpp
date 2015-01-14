14ced2da0d8be28d0179088a7ce36a40bfc50c88
Taranjeet <reachtotj@gmail.com>
2015-01-14 20:46:19 +0530

    double.cpp

---
+++ double.cpp
@@ -0,0 +1,34 @@
+
+/*
+2015-01-14 20:42
+practice
+JAN 15
+*/
+#include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+using namespace std;
+
+typedef long long int ll;
+
+#define FOR(i,n) for(int i = 0; i < n; i++)
+#define FORS(i,a,n) for(int i = a; i < n; i++)
+#define RDARR(a,n) FOR(i,n) cin>>a[i];
+#define SOLVE() int t;cin>>t;FOR(tc,t) solve();
+#define PB push_back
+
+void solve(){
+
+	int n;
+	cin>>n;
+	if(n&1)
+		cout<<n-1<<"\n";
+	else
+		cout<<n<<"\n";
+}
+
+int main(){ _
+
+	SOLVE()
+
+	return 0;
+}
