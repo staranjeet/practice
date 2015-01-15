cbe513b0fe35df5e20efb024555e219993c44ffa
Taranjeet <reachtotj@gmail.com>
2015-01-15 13:00:04 +0530

    remiss.cpp

---
+++ remiss.cpp
@@ -0,0 +1,32 @@
+
+/*
+2015-01-15 12:58
+practice
+JAN 15
+*/
+#include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+using namespace std;
+
+typedef long long int ll;
+
+#define FOR(i,n) for(int i = 0; i < n; i++)
+#define FORS(i,a,n) for(int i = a; i < n; i++)
+#define RDARR(a,n) FOR(i,n) cin>>a[i];
+#define SOLVE() int t;cin>>t;FOR(tc,t) solve();
+#define PB push_back
+
+void solve(){
+
+	int a,b;
+	cin>>a>>b;
+	cout<<max(a,b)<<" "<<a+b<<endl;
+}
+
+int main(){ _
+
+	SOLVE()
+
+
+	return 0;
+}
