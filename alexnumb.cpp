4261159be63da53626e4f5c3b213b63628e62e41
Taranjeet <reachtotj@gmail.com>
2015-01-16 11:31:53 +0530

    alexnumb.cpp

--- alexnumb.cpp
+++ alexnumb.cpp
@@ -1,32 +1,34 @@
 
 /*
+2015-01-16 11:30
 practice
-DEC 13
+JAN 15
 */
 #include <bits/stdc++.h>
 #define _ ios_base::sync_with_stdio(0);cin.tie(0);
 using namespace std;
 
-typedef long long ll;
+typedef long long int ll;
 
 #define FOR(i,n) for(int i = 0; i < n; i++)
 #define FORS(i,a,n) for(int i = a; i < n; i++)
+#define RDARR(a,n) FOR(i,n) cin>>a[i];
+#define SOLVE() int t;cin>>t;FOR(tc,t) solve();
+#define PB push_back
 
+void solve(){
+
+	ll n,x;
+	cin>>n;
+	FOR(i,n)
+		cin>>x;
+	x=(n*(n-1))/2;
+	cout<<x<<endl;
+}
 
 int main(){ _
-	int t;
-	cin>>t;
-	while(t--){
-	
-	 ll n,x,ans;
-	 cin>>n;
 
-	 FOR(i,n)
-	 cin>>x;
-
-	 ans=(n*(n-1))/2;
-	 cout<<ans<<"\n";
-	}
+	SOLVE()
 
 	return 0;
 }
