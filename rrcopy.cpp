2c230e59bb8d897cbb07fbe0d8648ea109c3c4d1
Taranjeet <reachtotj@gmail.com>
2015-01-16 11:44:54 +0530

    rrcopy.cpp

---
+++ rrcopy.cpp
@@ -0,0 +1,36 @@
+
+/*
+2015-01-16 11:43
+practice
+JAN 15
+*/
+#include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+using namespace std;
+
+typedef long long int ll;
+
+#define FOR(i,n) for(int i = 0; i < n; i++)
+#define FORS(i,a,n) for(int i = a; i < n; i++)
+#define RDARR(a,n) FOR(i,n) cin>>a[i];
+#define SOLVE() int t;cin>>t;FOR(tc,t) solve();
+#define PB push_back
+
+void solve(){
+
+	int n,x;
+	cin>>n;
+	set<int>num;
+	FOR(i,n){
+		cin>>x;
+		num.insert(x);
+	}
+	cout<<num.size()<<endl;
+}
+
+int main(){ _
+
+	SOLVE()
+
+	return 0;
+}
