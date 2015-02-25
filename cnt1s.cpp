027d2fa6e96d4015131886db424f084498cbf3c0
Taranjeet <reachtotj@gmail.com>
2015-02-25 12:49:42 +0530

    cnt1s.cpp

---
+++ cnt1s.cpp
@@ -0,0 +1,32 @@
+
+/*
+2015-02-25 12:48
+practice
+FEB 15
+*/
+#include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+using namespace std;
+
+typedef long long int ll;
+
+#define FOR(i,n) for(int i = 0; i < n; i++)
+#define FORS(i,a,n) for(int i = a; i < n; i++)
+#define RDARR(a,n) FOR(i,n) cin>>a[i];
+#define SOLVE() int t;cin>>t;FOR(tc,t) solve();
+#define PB push_back
+
+void solve(){
+	int n;
+	cin>>n;
+	bitset<64>b=n;
+	cout<<b.count()<<endl;
+}
+
+int main(){ _
+
+	//SOLVE()
+	solve();
+
+	return 0;
+}
