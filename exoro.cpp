14ea333d51af6364fb8b4fa458151507d80d8fdc
Taranjeet <reachtotj@gmail.com>
2015-03-12 22:04:32 +0530

    exoro.cpp

---
+++ exoro.cpp
@@ -0,0 +1,40 @@
+
+/*
+2015-03-12 22:01
+practice
+MAR 15
+*/
+#include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+using namespace std;
+
+typedef long long int ll;
+
+#define FOR(i,n) for(int i = 0; i < n; i++)
+#define FORS(i,a,n) for(int i = a; i < n; i++)
+#define RDARR(a,n) FOR(i,n) cin>>a[i];
+#define SOLVE() int t;cin>>t;FOR(tc,t) solve();
+#define PB push_back
+
+void solve(){
+
+	int n,odd=0,even=0,x;
+	cin>>n;
+	FOR(i,n){
+		cin>>x;
+		if(x&1)
+			odd++;
+
+	}
+	even=n-odd;
+	ll ans=odd*even;
+	cout<<ans<<endl;
+
+}
+
+int main(){ _
+
+	SOLVE()
+
+	return 0;
+}
