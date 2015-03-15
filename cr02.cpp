5b52a72ecf6b5b6a26abf6d9e30032ff45a4455d
Taranjeet <reachtotj@gmail.com>
2015-03-15 11:00:35 +0530

    cr02.cpp

---
+++ cr02.cpp
@@ -0,0 +1,40 @@
+
+/*
+2015-03-15 10:58
+practice
+MAR 15
+*/
+#include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+using namespace std;
+
+typedef long long int ll;
+
+#define FOR(i,n) for(int i = 0; i < n; i++)
+#define FORS(i,a,n) for(int i = a; i < n; i++)
+#define RDARR(a,n) FOR(i,n) cin>>a[i];
+#define SOLVE() int t;cin>>t;FOR(tc,t) solve();
+#define PB push_back
+
+void solve(){
+
+	int n;
+	cin>>n;
+	int mul=5,zero=0,temp;
+	while(1){
+		temp=n/mul;
+		if(temp<1)
+			break;
+		zero+=temp;
+		mul=mul*5;
+	}
+	cout<<zero<<endl;
+
+}
+
+int main(){ _
+
+	SOLVE()
+
+	return 0;
+}
