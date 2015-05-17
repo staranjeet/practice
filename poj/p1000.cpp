2d71cb586ef1edfebf97e08e84bdb038519d342a
Taranjeet <reachtotj@gmail.com>
2015-05-17 22:01:52 +0530

    p1000.cpp

---
+++ poj/p1000.cpp
@@ -0,0 +1,32 @@
+
+/*
+2015-05-17 21:54
+MAY 15
+*/
+//#include <bits/stdc++.h>
+//#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#include <iostream>
+using namespace std;
+
+typedef long long int ll;
+
+#define FOR(i,n) for(int i = 0; i < n; i++)
+#define FORS(i,a,n) for(int i = a; i < n; i++)
+#define RDARR(a,n) FOR(i,n) cin>>a[i];
+#define SOLVE() int t;cin>>t;FOR(tc,t) solve();
+#define PB push_back
+
+void solve(){
+
+	int a,b;
+	cin>>a>>b;
+	cout<<a+b<<endl;
+
+}
+
+int main(){ 
+
+	//SOLVE()
+	solve();
+	return 0;
+}
