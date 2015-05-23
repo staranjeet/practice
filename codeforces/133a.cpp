44ca61b05e94f8fb5196e120e2f5681767b06c6b
Taranjeet <reachtotj@gmail.com>
2015-05-23 11:57:56 +0530

    133a.cpp

---
+++ codeforces/133a.cpp
@@ -0,0 +1,35 @@
+
+/*
+2015-05-23 11:50
+MAY 15
+*/
+#include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+using namespace std;
+
+typedef long long int ll;
+
+#define FOR(i,n) for(int i = 0; i < n; i++)
+#define FORS(i,a,n) for(int i = a; i < n; i++)
+#define RDARR(a,n) FOR(i,n) cin>>a[i];
+#define SOLVE() int t;cin>>t;FOR(tc,t) solve();
+#define PB push_back
+
+void solve(){
+
+	string s;
+	cin>>s;
+	if(s.find("H")==string::npos && s.find("Q")==string::npos && s.find("9")==string::npos)
+		cout<<"NO\n";
+	else
+		cout<<"YES\n";
+
+
+}
+
+int main(){ _
+
+	//SOLVE()
+	solve();
+	return 0;
+}
